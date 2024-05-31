d3ca4709cf097da3108680e356bde06c95782d77
Diaz-adrianz <diazadrianz191205@gmail.com>
2024-05-31 23:14:59 +0700

    rename hook

--- next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'fr', 'de'],
-  },
-  localePath:
-    typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
-};
